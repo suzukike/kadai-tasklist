887fa03cfbb55695d510641542599690d03f693c
heihachi.jin <hinkawe3478@gmail.com>
2020-10-25 00:25:48 +0900

    Add NewServlet.java

--- src/models/Task.java
+++ src/models/Task.java
@@ -34,4 +34,36 @@ public class Task {
 
     @Column(name = "updated_at" , nullable = false)
     private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
